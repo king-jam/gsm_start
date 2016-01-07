bbd83991171fe0b846fcdc18f1f29ed2f8a1c312
James King <king.jam@husky.neu.edu>
2016-01-06 23:13:11 -0500

    Finished basic logic

--- gsm_start.ino
+++ gsm_start.ino
@@ -3,9 +3,12 @@
 
 #include "Adafruit_FONA.h"
 #include <EEPROM.h>
+const int EEPROM_MIN_ADDR = 0;
+const int EEPROM_MAX_ADDR = 511;
 
 #define FONA_RST 4
-
+#define FOB_LOCK 13
+#define FOB_START 14
 // this is a large buffer for replies
 char replybuffer[255];
 
@@ -22,6 +25,9 @@ void setup() {
   Serial.begin(115200);
   Serial.println(F("FONA SMS caller ID test"));
   Serial.println(F("Initializing....(May take 3 seconds)"));
+
+  pinMode(FOB_LOCK, OUTPUT);
+  pinMode(FOB_START, OUTPUT);
 
   // make it slow so its easy to read!
   fonaSerial->begin(4800);
@@ -50,7 +56,7 @@ void loop() {
     int slot = 0;            //this will be the slot number of the SMS
     int charCount = 0;
     uint16_t smslen;
-    bool stat = 
+    char password[20];
     //Read the notification into replybuffer
     do  {
       *bufPtr = fona.read();
@@ -77,27 +83,30 @@ void loop() {
       if (! fona.readSMS(slot, replybuffer, 250, &smslen)) {
         Serial.println("Failed to read SMS!");
       }
-      char* pass = readPassword();
+      char responseBuffer[255]; //we'll store the response here
+      
+      eepromReadPass(0,password, sizeof(password));
       char* recText = strtok(replybuffer, ":");
-      if (strcmp(recText, pass)) {
-        Serial.println
-      }
-      if (badMessage) {
-        // spam or failed message
-        
-      }
-      /// do actions to start car here!!!!!
-      /// do actions to start car here!!!!!
-
-      //Send back an automatic response
-      Serial.println("Sending reponse...");
-      if (true) {
-        stat = fona.sendSMS(replybuffer, "Success!");
+      if ((recText != NULL) && strcmp(recText, password)) {
+        recText = strtok(NULL, "");
+        if ((recText != NULL) && strcmp(recText, "start")) {
+          // successful command
+          if(startCar()) {
+            strcpy(responseBuffer, "Success");
+          } else {
+            strcpy(responseBuffer, "Start Failed");
+          }
+        } else {
+          strcpy(responseBuffer, "Bad Command - Failed");
+        }
       } else {
-        stat = fona.sendSMS(replybuffer, "Failed!");
+        strcpy(responseBuffer, "Bad Password or spam\n");
+        strcat(responseBuffer,replybuffer);
       }
       
-      if (!stat) {
+      //Send back an automatic response
+      Serial.println("Sending reponse...");
+      if (! fona.sendSMS(callerIDbuffer, responseBuffer)) {
         Serial.println(F("Failed"));
       } else {
         Serial.println(F("Sent!"));
@@ -115,13 +124,83 @@ void loop() {
   }
 }
 
-void writePassword(const char * password) {
-  EEPROM.put(0, password);
+boolean eepromAddrOk(int addr) {
+  return ((addr >= EEPROM_MIN_ADDR) && (addr <= EEPROM_MAX_ADDR));
 }
 
-char* readPassword() {
-  char* password;
-  EEPROM.get(0, password)
-  return password;
+boolean eepromWritePass(int startAddr, const byte* array, int numBytes) {
+  // counter
+  int i;
+  
+  // both first byte and last byte addresses must fall within
+  // the allowed range 
+  if (!eepromAddrOk(startAddr) || !eepromAddrOk(startAddr + numBytes)) {
+    return false;
+  }
+  
+  for (i = 0; i < numBytes; i++) {
+    EEPROM.write(startAddr + i, array[i]);
+  }
+  
+  return true;
 }
+
+boolean eepromReadPass(int addr, char* buffer, int bufSize) {
+  byte ch; // byte read from eeprom
+  int bytesRead; // number of bytes read so far
+  
+  if (!eepromAddrOk(addr)) { // check start address
+    return false;
+  }
+  
+  if (bufSize == 0) { // how can we store bytes in an empty buffer ?
+    return false;
+  }
+  
+  // is there is room for the string terminator only, no reason to go further
+  if (bufSize == 1) {
+    buffer[0] = 0;
+    return true;
+  }
+  
+  bytesRead = 0; // initialize byte counter
+  ch = EEPROM.read(addr + bytesRead); // read next byte from eeprom
+  buffer[bytesRead] = ch; // store it into the user buffer
+  bytesRead++; // increment byte counter
+  
+  // stop conditions:
+  // - the character just read is the string terminator one (0x00)
+  // - we have filled the user buffer
+  // - we have reached the last eeprom address
+  while ( (ch != 0x00) && (bytesRead < bufSize) && ((addr + bytesRead) <= EEPROM_MAX_ADDR) ) {
+    // if no stop condition is met, read the next byte from eeprom
+    ch = EEPROM.read(addr + bytesRead);
+    buffer[bytesRead] = ch; // store it into the user buffer
+    bytesRead++; // increment byte counter
+  }
+  
+  // make sure the user buffer has a string terminator, (0x00) as its last byte
+  if ((ch != 0x00) && (bytesRead >= 1)) {
+    buffer[bytesRead - 1] = 0;
+  }
+  
+  return true;
+}
+
+void pressFobButton(int pin, int onTime, int offTime) {
+  digitalWrite(pin, HIGH);
+  delay(onTime);
+  digitalWrite(pin, LOW);
+  delay(offTime);
+}
+
+boolean startCar() {
+  pressFobButton(FOB_LOCK, 200, 200);
+  pressFobButton(FOB_LOCK, 200, 200);
+  pressFobButton(FOB_START, 2000, 0);
+  //check some pin
+  return true;
+}
+
+
 
